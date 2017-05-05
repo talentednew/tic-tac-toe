cf9ad6d27529cf3afec52e9d7829062fa704ec91
BoogieJay <jw4100@nyu.edu>
2017-05-05 16:17:01 -0400

    refactor Board.java  and add comments

--- src/Board.java
+++ src/Board.java
@@ -2,21 +2,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Board class represents the board for human-AI competition
+ * It has a inner class "Cell" which stands for every square on the board
+ *
  * Created by BoogieJay
  * 4/23/17.
- *
- *
  */
 
 public class Board {
 
-    private final int SIZE = 4;
+    private static final int SIZE = 4;
 
     private final Cell[][] cells;
+
+    // combo to store the final winning combination(4 cells)
     private final List<int[]> combo;
+
     private int currentRow;
     private int currentCol;
 
+    /**
+     * the cell class represents the square on the board
+     */
     private class Cell {
 
         private Seed content;
@@ -38,9 +45,13 @@ public class Board {
         }
     }
 
+    /**
+     * board construction class
+     * initial all the cells on the board
+     */
     public Board () {
         this.cells = new Cell[SIZE][SIZE];
-        this.combo = new ArrayList<>(4);
+        this.combo = new ArrayList<>(SIZE);
 
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
@@ -49,6 +60,10 @@ public class Board {
         }
     }
 
+    /**
+     * check if the current board is tie
+     * @return result
+     */
     public boolean isTie () {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
@@ -60,6 +75,12 @@ public class Board {
         return true;
     }
 
+    /**
+     * check if "X" or "O" wins the game
+     *
+     * @param seed the seed
+     * @return result
+     */
     public boolean hasWin(Seed seed) {
 
         combo.clear();
@@ -69,8 +90,14 @@ public class Board {
         return !combo.isEmpty();
     }
 
+    /**
+     * check all winning conditions and return the final combo
+     * @param seed seed
+     * @return combo
+     */
     public List<int[]> checkConditionAndReturnCombo(Seed seed) {
 
+        // check row
         if (cells[currentRow][0].getContent() == seed
                 && cells[currentRow][1].getContent() == seed
                 && cells[currentRow][2].getContent() == seed
@@ -81,6 +108,7 @@ public class Board {
             combo.add(new int[]{currentRow, 3});
         }
 
+        // check column
         if (cells[0][currentCol].getContent() == seed
                 && cells[1][currentCol].getContent() == seed
                 && cells[2][currentCol].getContent() == seed
@@ -91,6 +119,7 @@ public class Board {
             combo.add(new int[]{3, currentCol});
         }
 
+        // check 45 degree diagonal
         if (currentCol == currentRow
                 && cells[0][0].getContent() == seed
                 && cells[1][1].getContent() == seed
@@ -102,6 +131,7 @@ public class Board {
             combo.add(new int[]{3, 3});
         }
 
+        // check 135 degree diagonal
         if (currentCol + currentRow == SIZE - 1
                 && cells[0][3].getContent() == seed
                 && cells[1][2].getContent() == seed
@@ -116,6 +146,10 @@ public class Board {
         return combo;
     }
 
+    /**
+     * *********** only for testing ***********
+     * print the board on the console
+     */
     public void paint() {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
@@ -128,18 +162,36 @@ public class Board {
         System.out.println();
     }
 
+    /**
+     * set current row
+     * @param row
+     */
     public void setCurrentRow(int row) {
         this.currentRow = row;
     }
 
+    /**
+     * set current column
+     * @param col
+     */
     public void setCurrentCol(int col) {
         this.currentCol = col;
     }
 
+    /**
+     * get the size of the board
+     * @return size
+     */
     public int getSIZE() {
         return this.SIZE;
     }
 
+    /**
+     * get content of cell desired
+     * @param row
+     * @param col
+     * @return seed
+     */
     public Seed getCell(int row, int col) {
         if (row < 0 || row >= SIZE || col < 0 || col >= SIZE) {
             throw new IllegalArgumentException("Error in row and col");
@@ -147,12 +199,21 @@ public class Board {
         return cells[row][col].getContent();
     }
 
+    /**
+     *  set content of cell desired
+     * @param row
+     * @param col
+     * @param seed
+     */
     public void setCell(int row, int col, Seed seed) {
         if (row < 0 || row >= SIZE || col < 0 || col >= SIZE) {
             throw new IllegalArgumentException("Error in row and col");
         }
+
+        // update current row and column for later checkConditionAndReturnCombo calling
         setCurrentRow(row);
         setCurrentCol(col);
+
         cells[currentRow][currentCol].setContent(seed);
     }
 
